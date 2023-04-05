cd0259f99621b80dd5ffd51bc62b45f6705e5626
Михаил <rotari733@gmail.com>
2023-04-05 23:04:22 +0300

    init RestApp

---
+++ src/test/java/ru/rotar/services/PeopleServiceTest.java
@@ -0,0 +1,24 @@
+package ru.rotar.services;
+
+import org.junit.jupiter.api.Test;
+import ru.rotar.repositories.PepleRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PeopleServiceTest {
+    private PepleRepository pepleRepository;
+    private PeopleService peopleService;
+
+    @Test
+    void faidAll() {
+
+    }
+
+    @Test
+    void findOne() {
+    }
+
+    @Test
+    void safe() {
+    }
+}
